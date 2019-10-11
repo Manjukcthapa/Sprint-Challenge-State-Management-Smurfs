2122390cc4d907953b42ca941a3dbb90dfb30b5d
Manju KC <kcmanjuthapa@gmail.com>
2019-10-11 11:24:51 -0400

    created action file and added fetchSmurfs function

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,10 +4,10 @@ import "./index.css";
 import App from "./components/App";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from 'redux-thunk';
+import reducer from "./store/reducer";
 
-import reducer from "./store/reducer/index";
-
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
