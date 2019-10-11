475e8512293e67fe2ec4563137bc83d4513f27b7
Manju KC <kcmanjuthapa@gmail.com>
2019-10-11 12:01:04 -0400

    fixed the error and data displayin screen

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,17 +1,15 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Smurfs from "../components/Smurfs";
 import { connect } from "react-redux";
 import fetchSmurfs from "../store/action";
 
 const SmurfList = props => {
-
   useEffect(() => {
     props.fetchSmurfs();
   }, []);
 
   return (
     <div>
-      <p>hello</p>
       {props.smurf &&
         props.smurf.map(sm => {
           return <Smurfs key={sm.id} smurf={sm} />;
--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -2,7 +2,6 @@ import React from "react";
 const Smurfs = props => {
   return (
     <div className="smurf">
-        <h1>hello</h1>
       <h3>{props.smurf.name}</h3>
       <p>Age: {props.smurf.age}</p>
       <p>Height: {props.smurf.height}</p>
