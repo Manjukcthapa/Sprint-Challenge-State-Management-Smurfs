088d03de856009bd32ab2fd50f76689b6ec44252
Manju KC <kcmanjuthapa@gmail.com>
2019-10-11 11:31:52 -0400

    added initialState item and fetching function in reducer

--- smurfs/src/store/reducer/index.js
+++ smurfs/src/store/reducer/index.js
@@ -1,14 +1,38 @@
 import {
-    FETCHING_SMURFS_START,
-    FETCHING_SMURFS_SUCCESS,
-    FETCHING_SMURFS_FAILURE,
-    ADDING_SMURF_SUCCESS
-  } from "../action/index";
+  FETCHING_SMURFS_START,
+  FETCHING_SMURFS_SUCCESS,
+  FETCHING_SMURFS_FAILURE,
+  ADDING_SMURF_SUCCESS
+} from "../action/index";
 
-const initialState = {};
+const initialState = {
+    smurfs: [],
+    isFetching: false,
+    error: ""
+
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING_SMURFS_START:
+      return {
+        ...state,
+        isFetching: true
+      };
+    case FETCHING_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        isFetching: false,
+        error: ""
+      };
+    case FETCHING_SMURFS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
+
     default:
       return state;
   }
