25885d1cd4a5c56e06ccc9085d4e49767ad9589f
Manju KC <kcmanjuthapa@gmail.com>
2019-10-11 11:54:39 -0400

    import fetchSmurfs and added useEffect function on SmurfList component and data render in screen

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Smurfs from "../components/Smurfs";
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
+import fetchSmurfs from "../store/action";
 
 const SmurfList = props => {
+
+  useEffect(() => {
+    props.fetchSmurfs();
+  }, []);
+
+  return (
     <div>
-    <p>hello</p>
-    {props.smurf &&
-      props.smurf.map(sm => {
-        return <Smurfs key={sm.id} smurf={sm} />;
-      })}
-  </div>
+      <p>hello</p>
+      {props.smurf &&
+        props.smurf.map(sm => {
+          return <Smurfs key={sm.id} smurf={sm} />;
+        })}
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
-    return {
-      smurf: state.smurfs
-    };
+  return {
+    smurf: state.smurfs
   };
+};
 
 export default connect(
-    mapStateToProps,
-    {  }
-  )(SmurfList);
+  mapStateToProps,
+  { fetchSmurfs }
+)(SmurfList);
