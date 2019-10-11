5993d6a52b546d6d5871970a4f7da1542c2679ff
Manju KC <kcmanjuthapa@gmail.com>
2019-10-11 11:42:37 -0400

    imported connect

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Smurfs from "../components/Smurfs";
+import {connect} from 'react-redux'
 
 const SmurfList = props => {
-  <div>
-    <Smurfs />
-  </div>;
+    <div>
+    <p>hello</p>
+    {props.smurf &&
+      props.smurf.map(sm => {
+        return <Smurfs key={sm.id} smurf={sm} />;
+      })}
+  </div>
 };
+
+
 
 export default SmurfList;
